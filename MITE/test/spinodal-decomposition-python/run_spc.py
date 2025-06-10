cb6ed237eed0061706525e6d97f3769c976f4d83
Wennie Wang <wwwennie@gmail.com>
2025-06-09 19:18:03 -0500

    Delete MITE/test directory

--- MITE/test/spinodal-decomposition-python/run_spc.py
+++
@@ -1,8 +0,0 @@
-from spinodal_decomp import *
-
-# Inputs
-#    n (integer) = array size, 
-#    nstep (integer) = number of iterations, 
-#    d (float) = diffusivity, AA (float) = non-linear interaction parameter
-sim = SpinodalConserved(n=100, nstep=300, d=0.5, AA = 1.3)
-sim.animate()
--- MITE/test/spinodal-decomposition-python/run_spnc.py
+++
@@ -1,9 +0,0 @@
-from spinodal_decomp import *
-
-# Inputs
-#    n (integer) = array size, 
-#    nstep (integer) = number of iterations, 
-#    d (float) = diffusivity, AA (float) = non-linear interaction parameter
-
-sim = SpinodalNonConserved(n=100, nstep=300, d=0.5, AA = 1.3)
-sim.animate()
